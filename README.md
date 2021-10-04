8855e279dbe175c40f98593f88e0d88e89401898
Joan-SHI <qiongnashi@gmail.com>
2021-10-05 02:16:45 +1300

    play around react-bootstrap TypeScript

--- README.md
+++ README.md
@@ -8,10 +8,14 @@ https://create-react-app.dev/docs/getting-started
 
 <a href="https://www.linkedin.com/in/joan-shi-87a152179/" target="_blank"><img src="./my-app/src/development.png" alt="Joan's mirror image at Museum of NZ Te Papa Tongarewa" width="750" /></a>
 
-# :icecream: Adding TypeScript
+# :icecream: Add TypeScript
 
 https://create-react-app.dev/docs/adding-typescript/
 
-# :icecream: Adding type definitions for react-bootstrap
+# :icecream: Add type definitions for react-bootstrap
 
 https://www.npmjs.com/package/@types/react-bootstrap
+
+# :icecream: Play around React-Bootstrap TypeScript 
+
+https://codesandbox.io/s/github/react-bootstrap/code-sandbox-examples/tree/master/basic-ts?file=/src/App.tsx
---
+++ my-app/src/App.tsx
@@ -0,0 +1,71 @@
+import logo from './mirror.jpg';
+import './App.css';
+
+// function App() {
+//   return (
+    // <div className="App">
+    //   <header className="App-header">
+        
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       施琼娜的简历
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://www.linkedin.com/in/joan-shi-87a152179/"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn about Joan
+    //     </a>
+    //   </header>
+    // </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react';
+
+// import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+
+import ButtonsShowcase from './showcases/Buttons';
+import ToastsShowcase from './showcases/Toasts';
+
+const App: React.FC = () => {
+  return (
+    
+      <header className="App-header">
+        
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          施琼娜的简历
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/joan-shi-87a152179/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn about Joan
+        </a>
+        <Container className="p-3">
+      {/* <Jumbotron>
+        <h1 className="header">
+          Welcome To React-Bootstrap TypeScript Example
+        </h1>
+      </Jumbotron> */}
+      <h2>Buttons</h2>
+      <ButtonsShowcase />
+      <h2>Toasts</h2>
+      <ToastsShowcase />
+    </Container>
+      </header>
+    
+    
+  );
+};
+
+export default App;
